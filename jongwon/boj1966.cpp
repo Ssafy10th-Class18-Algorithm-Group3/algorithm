0d15f4caf20629a4ddbb3f7113b53695cd0975af
jongwon <hgw4846@gmail.com>
2023-07-27 16:32:13 +0900

    thie week algorithm problem solving

---
+++ jongwon/boj1966.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <deque>
+#include <algorithm>
+using namespace std;
+
+
+
+int main() {
+	int N, tc, M;
+	deque <int> imp;
+	cin >> tc;
+	for (int i = 0; i < tc; i++) {
+		cin >> N >> M; // 문서의 갯수 N 현재 큐에서의 위치 M 0부터 시작
+		int cur;
+		int target;
+		for (int j = 0; j < N; j++) {
+			cin >> cur;
+			imp.push_back(cur);
+		}
+		int ans = 0;
+		int cur_num;
+		while(1) {
+			cur_num = imp.front();
+			if (cur_num == *max_element(imp.begin(), imp.end())) {
+				imp.pop_front();
+				ans++;
+				if (M == 0) {
+					break;
+				}
+			}
+			else {
+				imp.pop_front();
+				imp.push_back(cur_num);
+                for(auto iter:imp){
+                    cout << iter;
+                }
+                cout <<endl;
+				M--;
+				if (M < 0) {
+					M = imp.size()-1;
+				}
+			}
+
+
+		}
+		cout << ans << "\n";
+
+	}
+
+
+}
---
+++ jongwon/boj2346.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+	int N;
+	cin >> N;
+	int *info = new int[N];
+	bool *check = new bool[N];
+	for (int i = 0; i < N; i++) {
+		cin >> info[i];
+	}
+	fill(check, check + N, false);
+	vector <int >ans;
+	int idx = 0;
+	int target;
+	while (1) {
+		target = info[idx];
+		ans.push_back(idx + 1);
+		check[idx] = true;
+		if (ans.size() == N) {
+			break;
+		}
+		if (target > 0) {
+			int cur = 0;
+			while (1) {
+				idx++;
+				if (idx >= N) {
+					idx = idx - N;
+				}
+				if (!check[idx]) {
+					cur++;
+				}
+				if (cur == target) {
+					break;
+				}
+			}
+		}
+		else {
+			int cur = 0;
+			while (1) {
+				idx--;
+				if (idx < 0) {
+					idx = N + idx;
+				}
+				if (!check[idx]) {
+					cur--;
+				}
+				if (cur == target) {
+					break;
+				}
+			}
+		}
+	}
+	for (auto iter : ans) {
+		cout << iter << " ";
+	}
+
+}
